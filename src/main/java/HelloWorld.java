de86c5b258e6e09bbb42d5dd85197c5eee15abb6
jiangzhiru <jackjzr@hotmail.com>
2018-04-03 17:40:35 +0800

    test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,4 +4,7 @@ package main.java;
  * Created by jiangzhiru on 18/4/3.
  */
 public class HelloWorld {
-}
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+    }}
--- src/main/java/HelloWorld1.java
+++ src/main/java/HelloWorld1.java
@@ -7,6 +7,6 @@ public class HelloWorld1 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        System.out.println("Hello world1");
     }
 }
